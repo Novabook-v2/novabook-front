90e0d6bf24bc7ac9b48bafa0bc51e34a655626b9
2-say <dev2say@gmail.com>
2025-02-07 15:50:53 +0900

    feat: aws 배포환경 구성

--- src/main/java/store/novabook/front/api/order/naver/BookSearchService.java
+++ src/main/java/store/novabook/front/api/order/naver/BookSearchService.java
@@ -1,6 +1,7 @@
 package store.novabook.front.api.order.naver;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,14 @@ import store.novabook.front.common.util.dto.NaverSearchDto;
 public class BookSearchService {
 
 	private final NaverBookSearchApiClient naverBookSearchApiClient;
+
+	@Value("${NAVER-SEARCH-PASSWORD}")
+	private String NaverSearchApiPassword;
+
+	@Value("${NAVER-SEARCH-ID}")
+	private String NaverSearchApiId;
+
 	public String searchBooks(String query) {
-		return naverBookSearchApiClient.getSearch("aY3htNCRURon01pEVu8z", "DWSGBOBel9", query, 5, 1);
+		return naverBookSearchApiClient.getSearch(NaverSearchApiId, NaverSearchApiPassword, query, 5, 1);
 	}
 }
